bcd410d3e091476c98df3e6c0096e5ffc5b7debd
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-08-26 19:46:39 +0300

    Newest changes

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+== TODO ==
+
+ * Kaksi testivaihtoehtoa: A) kuvan perusteella arvomerkin nimi, B) arvomerkin nimen perusteella kuva
+ * Painotus merkkien arvontaan vaikeuden mukaan
+
+ 
--- main.js
+++ main.js
@@ -1,6 +1,60 @@
 
 require(["jquery"], function($) {
+	
+	function get_img(type) {
+		return "graphics/wikipedia/" + type[0] + "_" + type[1] + ".svg";
+	}
+	
+	function get_option() {
+		var items = [
+			"Kenraali", 
+			"Kenraaliluutnantti", 
+			"Kenraalimajuri", 
+			"Prikaatikenraali", 
+			"Eversti",
+			"Everstiluutnantti",
+			"Majuri",
+			"Kapteeni",
+			"Yliluutnantti",
+			"Luutnantti",
+			"V‰nrikki",
+			"Sotilasmestari",
+			"Yliv‰‰peli",
+			"V‰‰peli",
+			"Ylikersantti",
+			"Upseerikokelas",
+			"Kersantti",
+			"Upseerioppilas",
+			"Alikersantti",
+			"Korpraali",
+			"Aliupseerioppilas",
+			"Sotamies"
+		];
+		return items[Math.floor(Math.random()*items.length)];
+	}
+	
+	function init_test() {
+		var items = [get_option(), get_option(), get_option(), get_option()];
+		var right_item = Math.floor(Math.random()*items.length);
+		
+		$("#test_content .hint").text(items[right_item]);
+
+		for(i in items) {
+			if(i === right_item) {
+				$("#test_content .link_option" + (i+1)).click(function() {
+					alert("OK");
+				});
+			} else {
+				$("#test_content .link_option" + (i+1)).click(function() {
+					alert("FAIL");
+				});
+			}
+			$("#test_content .img_option" + (i+1)).attr("src", get_img([items[i], "kauluslaatta"]));
+		}
+		
+	}
+	
     $(function() {
-        $('body').css('background-color', '#000000');
+		init_test();
     });
 });

ec0307eaf762dd110005bd0d3c0f97bb1e8cee6f
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-09-02 16:16:08 +0300

    Updated application

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-
+Ôªø
 require(["jquery"], function($) {
+	
+	var stats = {
+		'tries': 0,
+		'successes': 0,
+		'fails': 0
+	};
 	
 	function get_img(type) {
 		return "graphics/wikipedia/" + type[0] + "_" + type[1] + ".svg";
 	}
 	
-	function get_option() {
+	function get_options(size) {
 		var items = [
 			"Kenraali", 
 			"Kenraaliluutnantti", 
@@ -17,10 +23,10 @@ require(["jquery"], function($) {
 			"Kapteeni",
 			"Yliluutnantti",
 			"Luutnantti",
-			"V‰nrikki",
+			"V√§nrikki",
 			"Sotilasmestari",
-			"Yliv‰‰peli",
-			"V‰‰peli",
+			"Yliv√§√§peli",
+			"V√§√§peli",
 			"Ylikersantti",
 			"Upseerikokelas",
 			"Kersantti",
@@ -30,31 +36,58 @@ require(["jquery"], function($) {
 			"Aliupseerioppilas",
 			"Sotamies"
 		];
-		return items[Math.floor(Math.random()*items.length)];
+		function randOrd(){ return (Math.round(Math.random())-0.5); }
+		items.sort( randOrd );
+		var ret = [];
+		for(var i=0; i!=size; i++) {
+			ret.push(items.shift());
+		}
+		return ret;
 	}
 	
 	function init_test() {
-		var items = [get_option(), get_option(), get_option(), get_option()];
-		var right_item = Math.floor(Math.random()*items.length);
-		
-		$("#test_content .hint").text(items[right_item]);
-
-		for(i in items) {
-			if(i === right_item) {
-				$("#test_content .link_option" + (i+1)).click(function() {
-					alert("OK");
-				});
+		function end_game(success) {
+			//alert("end_game()");
+			stats.tries++;
+			if(success) {
+				stats.successes++;
 			} else {
-				$("#test_content .link_option" + (i+1)).click(function() {
-					alert("FAIL");
-				});
+				stats.fails++;
 			}
-			$("#test_content .img_option" + (i+1)).attr("src", get_img([items[i], "kauluslaatta"]));
+			content.remove();
+			init_test();
+		}
+		var content = $("#test_content").clone().show();
+		content.appendTo('#content');
+		var items = get_options(4);
+		var right_item = 1 + Math.floor(Math.random()*items.length);
+		//alert("right_item = " + right_item);
+		content.find(".hint").text(''+items[right_item-1]);
+		
+		var nn = 0;
+		for(i in items) {
+			nn = nn + 1;
+			//alert( nn + " == " + items[i]);
+			(function(n) {
+				if(n === right_item) {
+					content.find(".link_option" + n).click(function() {
+						end_game(true);
+					});
+				} else {
+					content.find(".link_option" + n).click(function() {
+						end_game(false);
+					});
+				}
+				content.find(".img_option" + n).attr("src", get_img([items[i], "kauluslaatta"]));
+			})(nn);
 		}
 		
+		$("#stats .successes").text(stats.successes);
+		$("#stats .fails").text(stats.fails);
 	}
 	
     $(function() {
+		$("#test_content").hide();
 		init_test();
     });
 });
